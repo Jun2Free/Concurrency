ef45b89c9ec4fd5fcd22032f08788baccada9f7c
Jun2Free <c16618@naver.com>
2022-06-28 17:10:43 -0400

    edit comments about compiler

--- secondThread.cpp
+++ secondThread.cpp
@@ -1,3 +1,7 @@
+// g++ secondthread.cpp -pthread
+// f you compile without the -pthread flag, you will see an error of the form: undefined reference to pthread_create.
+// // You will need to use the -pthread flag for all other multithreaded examples in this course going forward.
+
 #include <iostream>
 #include <thread>
 

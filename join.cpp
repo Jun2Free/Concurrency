a62d629ec8fe635f2f94ff11f0500bbae901aea0
Jun2Free <c16618@naver.com>
2022-06-28 17:06:32 -0400

    how to make a second thread & how to use 'thread::join'

---
+++ join.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+void pause_thread(int n)
+{
+    std::this_thread::sleep_for (std::chrono::seconds(n));
+    std::cout << "pause of " << n << " seconds ended\n";
+}
+
+int main()
+{
+    std::cout<< "Spawning 3 threads...\n";
+    std::thread t1 (pause_thread,1);
+    std::thread t2 (pause_thread,2);
+    std::thread t3 (pause_thread,3);
+    std::cout << "Done spawning threads. Now waiting for them to join:\n";
+
+    t1.join();
+    t2.join();
+    t3.join();
+    std::cout << "All threads joined!\n";
+
+    return 0;
+
+}
